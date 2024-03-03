391056e5d3e5b878390113a505665fda0bde3ccf
softlee144 <ockjungbong@gmail.com>
2024-03-03 21:03:14 +0900

    Layout  분할

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,15 +1,10 @@
-import { Link } from "react-router-dom";
+import BasicLayout from "../layouts/BasicLayout";
 
 const MainPage = () => {
   return (
-    <div>
-      <div className="flex">
-        <Link to={"/about"}>About</Link>
-      </div>
-      <div className="text-3xl">
-        <div>Main Page</div>
-      </div>
-    </div>
+    <BasicLayout>
+      <div className="text-3xl">Main Page</div>
+    </BasicLayout>
   );
 };
 
