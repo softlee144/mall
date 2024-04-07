57ce4c1d33ca9e1c8ec35afae9bb2578ffc3b814
softlee144 <ockjungbong@gmail.com>
2024-04-07 23:40:40 +0900

    리다이렉션 처리 - 추가된 설정으로 인해 브라우저에서 'Todo' 메뉴를 선택하거나 /todo/ 경로를 호출하는 경우 자동으로 /todo/list로 이동한다.

--- src/router/todoRouter.js
+++ src/router/todoRouter.js
@@ -3,9 +3,6 @@ import { Navigate } from "react-router-dom";
 
 const Loading = <div>Loading....</div>;
 const TodoList = lazy(() => import("../pages/todo/ListPage"));
-// const TodoRead = lazy(() => import("../pages/todo/ReadPage"));
-// const TodoAdd = lazy(() => import("../pages/todo/AddPage"));
-// const TodoModify = lazy(() => import("../pages/todo/ModifyPage"));
 
 const todoRouter = () => {
   return [
@@ -21,30 +18,6 @@ const todoRouter = () => {
       path: "",
       element: <Navigate replace to="list" />,
     },
-    /* {
-      path: "read/:tno",
-      element: (
-        <Suspense fallback={Loading}>
-          <TodoRead />
-        </Suspense>
-      ),
-    },
-    {
-      path: "add",
-      element: (
-        <Suspense fallback={Loading}>
-          <TodoAdd />
-        </Suspense>
-      ),
-    },
-    {
-      path: "modify/:tno",
-      element: (
-        <Suspense fallback={Loading}>
-          <TodoModify />
-        </Suspense>
-      ),
-    }, */
   ];
 };
 
