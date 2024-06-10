da5dec869e890d7956162933a1babb4863ab64d0
softlee144 <ockjungbong@gmail.com>
2024-06-11 00:40:10 +0900

    수정 삭제 컴포넌트 기능 추가

--- src/pages/todo/ModifyPage.js
+++ src/pages/todo/ModifyPage.js
@@ -1,22 +1,14 @@
-/*
- * 수정의 경우 수정된 결과를 확인할 수 있는 조회 화면으로 다시 이동이 가능해야 하고,
-   삭제의 경우 목록 화면으로 이동하는 경우가 많다. 
-   이를 위한 기능들을 useNavigate() 를 이용해서 작성한다.
- */
+import { useParams } from "react-router-dom";
+import ModifyComponent from "../../components/todo/ModifyComponent";
 
-import { useNavigate } from "react-router-dom";
-
-const ModifyPage = ({ tno }) => {
-  const navigate = useNavigate();
-
-  const moveToRead = () => {
-    navigate({ pathname: "/todo/read/${tno}" });
-  };
-
-  const moveToList = () => {
-    navigate({ pathname: "/todo/list" });
-  };
-  return <div className="text-3xl font-extrabold">Todo Modify Page</div>;
+const ModifyPage = () => {
+  const { tno } = useParams();
+  return (
+    <div className="p-4 w-full bg-white">
+      <div className="text-3xl font-extrabold">Todo Modify Page</div>
+      <ModifyComponent tno={tno} />
+    </div>
+  );
 };
 
 export default ModifyPage;
